85790c8de2032d4f4d921c65a48e0b4d541f158d
Manuel Ernesto Chang Arámbulo <mchang0506@gmail.com>
2019-08-07 21:00:26 -0400

    Finish styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from '@material-ui/styles';
 const styles = {
   root: {
     backgroundColor: "blue",
-    height: "100%",
+    height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center"
@@ -44,9 +44,7 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <p>
-                <MiniPalette {...palette} />
-              </p>
+              <MiniPalette {...palette} />
             ))}
           </div>
         </div>
